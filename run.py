3678faf1122633cf9008a4b533d6c9773b7d68bc
Exel Boy Alfanso <72937148+ExelCoeg@users.noreply.github.com>
2025-06-06 14:43:58 +0700

    fix: cors policy on run.py gunicorn

--- run.py
+++ run.py
@@ -1,3 +1,6 @@
 from myapp import create_app
+from flask_cors import CORS
 
 app = create_app()
+
+CORS(app, supports_credentials=True,origins=["http://127.0.0.1:5500", "https://awarely-black.vercel.app/"])
